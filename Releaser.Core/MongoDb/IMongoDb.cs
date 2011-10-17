60d7363b3f14a5108d54a90225b3f272a1a5ea3c
Pavel Belousov <pavel.mephi@gmail.com>
2011-10-17 20:09:51 +0400

    Class for working with MongoDb was added.

---
+++ Releaser.Core/MongoDb/IMongoDb.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using MongoDB.Driver;
+
+namespace Releaser.Core.MongoDb
+{
+	/// <summary>
+	/// Interface for working with MongoDb database.
+	/// </summary>
+	public interface IMongoDb
+	{
+		/// <summary>
+		/// Saves entity to database.
+		/// </summary>
+		void SaveEntity<T>(T entity);
+
+		/// <summary>
+		/// Saves entities to database.
+		/// </summary>
+		void SaveEntities<T>(IEnumerable<T> entities);
+
+		/// <summary>
+		/// Gets entity from database.
+		/// </summary>
+		T GetEntity<T>(IMongoQuery query);
+
+		/// <summary>
+		/// Gets entities from database.
+		/// </summary>
+		MongoCursor<T> GetEntities<T>(IMongoQuery query);
+
+		/// <summary>
+		/// Deletes entity from database.
+		/// </summary>
+		void DeleteEntity<T>(IMongoQuery query);
+	}
+}
